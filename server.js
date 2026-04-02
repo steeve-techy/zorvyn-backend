4c8d5f4bb364249e4dacc21d7b8ccef056116c15
STEEVE C BABY <steevecbaby@gmail.com>
2026-04-02 23:34:34 +0530

    Final fixes: MongoDB Atlas connection and deployment ready

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require("dotenv").config(); // 🔥 ADD THIS LINE
+
 const app = require("./app");
 const connectDB = require("./config/db");
 
 // connect DB
 connectDB();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
